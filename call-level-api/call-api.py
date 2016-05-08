c776145876881eb5fa426648250f67d8535d05d1
Jérôme Lemaire <jejelemaire@gmail.com>
2016-05-08 14:52:49 +0200

    Update readme + essai print ticket

--- call-level-api/call-api.py
+++ call-level-api/call-api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.4
+#coding: utf8
 #
-# Small script to show PostgreSQL and Pyscopg together
 #
 
 import psycopg2
@@ -9,9 +9,9 @@ import psycopg2
 ##################### Connection to the database #####################
 
 try:
-    conn = psycopg2.connect("dbname='base' user='dbuser' host='localhost' password=''")
+    conn = psycopg2.connect("dbname='base' user='postgres' host='localhost' password=''")
 except:
-    print "I am unable to connect to the database"
+    print ("I am unable to connect to the database")
 
 
 cur = conn.cursor()
@@ -24,36 +24,54 @@ cur.callproc('AcquireTable', [635614])
 token = cur.fetchone()[0]
 print(token)
 
+
 ################# Order spakling water ##################
 
 
 queryOrderOneSpaklingWater = "SELECT OrderDrinks("+str(token)+", ARRAY[('6',1)] :: orderList[]);"
 
+queryOrderOneother = "SELECT OrderDrinks("+str(token)+", ARRAY[('4',1)] :: orderList[]);"
+
 
 cur.execute(queryOrderOneSpaklingWater)
 order = cur.fetchone()[0]
-print('order n°')
-print(order)
+print'order n°{}'.format(order)
+
 
 #################### Looks bill ####################
 
 cur.callproc('IssueTicket', [token])
-bill = cur.fetchone()[0]
+bill = cur.fetchall()[0]
 
-print(bill)
+print 'bill {0}'.format(bill)
+
+
 
 
 ############# Order sparkling water ##############
 
 cur.execute(queryOrderOneSpaklingWater)
 order = cur.fetchone()[0]
-print('order n°')
-print(order)
+print('order n°{}').format(order)
+
+cur.execute(queryOrderOneother)
+order = cur.fetchone()[0]
+print('order n°{}').format(order)
+
+cur.callproc('IssueTicket', [token])
+bill = cur.fetchall()[0]
+
+amount = bill[0]
+drinks = bill[1]
+print 'amount {0}'.format(amount)
+print 'drinks {0}'.format(drinks)
+
+
 
 
 ############# Pay and realese table ##############
 
-cur.callproc('PayTable', [token, '3.23'])
+cur.callproc('PayTable', [token, '13.23'])
 
 conn.commit()
 
